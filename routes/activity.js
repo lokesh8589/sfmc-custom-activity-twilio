df167c0f1a4a02347c5cc691b49b34de6126a66a
lokesh8589 <52921027+lokesh8589@users.noreply.github.com>
2022-02-11 14:17:21 +0530

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -119,7 +119,7 @@ exports.execute = function (req, res) {
     client.messages 
           .create({ 
              body: body,
-             from :'+13203473736',
+             from :'+8304026371',
              to: '+91'+to 
            }) 
            .then(message => console.log(message.sid)) 
